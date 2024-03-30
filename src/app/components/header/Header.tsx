2352d9960a4466cb2f528eb9eb64e184bbb25512
Kacper Mikolajczyk <vkacperpl@gmail.com>
2024-03-30 16:13:50 +0100

    finished

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -8,14 +8,14 @@ import Image from "next/image";
 
 const Header = () => {
     return(
-        <div className="flex justify-center font-gilroy mt-[50px]">
-            <Image className="flex mt-[15px]" width={100} src={Logo} alt="logo" loading="lazy"/>
-            <div className="flex w-[300px] h-[30px] bg-black border-2 border-[#62cb5d] rounded-xl m-[5px] mt-[50px] ml-[250px]">
+        <div className="flex justify-center font-gilroy mt-[50px] max-[500px]:grid">
+            <Image className="flex mt-[15px] max-[500px]:ml-auto max-[500px]:mr-auto" width={100} src={Logo} alt="logo" loading="lazy"/>
+            <div className="flex w-[300px] h-[30px] bg-black border-2 border-[#62cb5d] rounded-xl m-[5px] mt-[50px] ml-[250px] max-[980px]:hidden">
                 <Image className="m-[5px]" src={Search} alt="Search" width={16} height={16} loading="lazy"/>
-                <input className="bg-black text-white w-[260px] text-[14px] active:no-underline" type="text" />
+                <input className="bg-black text-white w-[260px] text-[14px] active:no-underline max-[980px]:hidden" type="text" />
                 <Image className="m-[5px]" src={Globe} alt="Globe" width={16} height={16} loading="lazy"/>
             </div>
-            <div className="mt-[35px] flex ml-[150px] font-gilroy_600">
+            <div className="mt-[35px] flex ml-[150px] font-gilroy_600 max-[500px]:ml-auto">
                 <div className="flex" onClick={(() => location.href = 'https://www.productgem.co/sign-in')}>
                     <Image width={32} height={32} className="w-[32px] h-[32px] mt-[14px]" src={LoginArrow} alt="Login Arrow" loading="lazy"/>
                     <button className="text-white h-[35px] m-[12px]">Login</button>
