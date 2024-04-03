b5838ae40b36f42462bb32a1f3372e9e139c7c91
Kacper Mikolajczyk <vkacperpl@gmail.com>
2024-04-03 16:45:01 +0200

    Footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Youtube from "@/app/assets/youtube.png";
 import LinkedIn from "@/app/assets/linkedin.png";
 import Instagram from "@/app/assets/instagram.png";
@@ -11,21 +12,17 @@ const Footer = () => {
         <div className="flex justify-center font-gilroy mt-[200px] max-[870px]:grid">
             <div className="m-[80px]">
                 <p className="text-white font-gilroy_600 max-[350px]:text-center">SUPPORT</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Submit Ticket</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">FAQs</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Help</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Knowledge</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Forum</p>
+                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center" onClick={(() => location.href = 'https://discord.gg/productgem')}>Submit Ticket</p>
+                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center" onClick={(() => location.href = '#faq')}>FAQs</p>
+                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center" onClick={(() => location.href = 'https://www.productgem.co/contact')}>Help</p>
             </div>
             <div className="m-[80px]">
                 <p className="text-white font-gilroy_600 max-[350px]:text-center">COMPANY</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">About Us</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Professional Team</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Newsroom</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Careers</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Partners</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Blog</p>
-                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center">Contact Us</p>
+                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center" onClick={(() => location.href = 'https://www.productgem.co/terms-of-service')}>Terms of service</p>
+                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center" onClick={(() => location.href = 'https://www.productgem.co/refund-policy')}>Refund Policy</p>
+                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center" onClick={(() => location.href = 'https://www.productgem.co/privacy-policy')}>Privacy Policy</p>
+                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center" onClick={(() => location.href = 'https://www.productgem.co/contact')}>Contact</p>
+                <p className="text-slate-300 mt-[15px] hover:cursor-pointer max-[350px]:text-center" onClick={(() => location.href = 'https://discord.gg/productgem')}>Carrers</p>
             </div>
             <div className="m-[80px]">
                 <p className="text-white font-gilroy_600 max-[350px]:text-center">GET SPECIAL OFFER FROM US</p>
